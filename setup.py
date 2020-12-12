bcdfda899b9530d05be5338dca187a29c08a96cf
Dominik Maksymilian Nowak <d.m.nowak@stud.elka.pw.edu.pl>
2020-12-12 20:07:55 +0100

    no

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname, encoding='utf-8')).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 
 setup(
